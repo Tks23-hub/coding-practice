511eef32dc1c56ae32fda114dc0f51e15e629840
Mohamed Soliman <forcodin23@gmail.com>
2025-04-27 15:25:08 +0300

    Sum of 2 number in an array should can be equal to the target number

---
+++ two-sum-in-array/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {   
+                    int[] result = new int[2];
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return new int[0]; 
+    }
+}
