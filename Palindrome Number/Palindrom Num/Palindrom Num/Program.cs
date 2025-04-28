a8a4fc4142196e9110690937c6e62ae0363db8e2
Mohamed Soliman <forcodin23@gmail.com>
2025-04-28 13:43:35 +0300

    checking if the recevied number is Palindrom number (a number that stays the same when its reversed)

---
+++ Palindrome Number/Palindrom Num/Palindrom Num/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    int x = 112211;
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        bool result = solution.IsPalindrome(solution.x);
+        Console.WriteLine(result); 
+    }
+    public bool IsPalindrome(int x)
+    {
+        int reverse = 0;
+        int original = x;
+        while(x > 0)
+        {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x /= 10;
+        }
+        return original == reverse;
+    }
+}
